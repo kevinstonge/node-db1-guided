3639ff389c23b850b4095b7d92376d2a42c44336
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-02 19:49:02 +0100

    wip

--- posts/post-router.js
+++ posts/post-router.js
@@ -57,7 +57,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   Posts.create(req.body)
-    .then(data => {
+    .then(([id]) => {
       res.json(data)
     })
     .catch(error => {
