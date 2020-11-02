1b9d217d2c7c9e30dc32b91339c52c226786cd2d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-02 20:12:13 +0100

    wip

--- posts/post-router.js
+++ posts/post-router.js
@@ -81,7 +81,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const unknownThing = await Posts.delete(req.params.id)
-    res.json(unknownThing)
+    res.json({ message})
   } catch (error) {
     res.json({ message: error.message })
   }
